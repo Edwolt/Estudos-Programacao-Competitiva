4f1f2ef1006e8bd6ddac96f72bce9921da980f1e
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-09 13:07:29 -0300

    Fubonacci Recursivo
    Foi adicionado o código do fibonacci recursivo

---
+++ Fibonacci/recursivo.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll unsigned long long
+
+ll fibonacci(int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+}
