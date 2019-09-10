caa86d538b1f20f49ef394f2cd076f9ceb4dcfc7
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-10 15:43:36 -0300

    Grafo: Grafo
    Arquivo com a forma que iremos representar os gravos

---
+++ Grafos/grafo.cpp
@@ -0,0 +1,22 @@
+// Forma de repesentar um grafo
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e5;
+
+int n, m; // Número de celulas e número de arestas
+vector<int> grafo[MAXN];
+
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        grafo[u].push_back(v);
+        grafo[v].push_back(u); // Grafo não orientado
+    }
+}
