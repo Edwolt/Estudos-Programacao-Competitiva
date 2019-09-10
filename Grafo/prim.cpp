46c99702fa4573217a93a7a9a162190adba1917c
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-10 19:57:27 -0300

    Grafo: Prim

---
+++ Grafo/prim.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e6;
+
+struct edge
+{
+    int vertice, peso;
+    bool operator<(const edge &that) const
+    {
+        return this->peso > that.peso;
+    }
+};
+
+int n, m;
+
+bool vis[MAXN];
+vector<edge> grafo[MAXN];
+
+int prim()
+{
+    priority_queue<edge> pq;
+
+    for (edge i : grafo[0])
+    {
+        pq.push(i);
+    }
+    vis[0] = true;
+    int total = 0;
+
+    while (!pq.empty())
+    {
+        edge e = pq.top();
+        pq.pop();
+
+        if (!vis[e.vertice])
+        {
+            vis[e.vertice] = true;
+            total += e.peso;
+
+            for (edge i : grafo[e.vertice])
+            {
+                pq.push(i);
+            }
+        }
+    }
+    return total;
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    int u, v, w;
+    for (int i = 0; i < m; i++)
+    {
+        vis[i] = false;
+        cin >> u >> v >> w;
+        grafo[u].push_back({v, w});
+        grafo[v].push_back({u, w});
+    }
+
+    int res = prim();
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            cout << -1 << endl;
+            return 0;
+        }
+    }
+
+    cout << res << endl;
+}
