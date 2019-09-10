d5ded1517d216007be99ffc36ad5b0a3bb428140
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-10 15:52:14 -0300

    Grafos: DFS

---
+++ Grafos/dfs.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e5;
+
+int n, m; // Número de vertices e número de arestas
+vector<int> grafo[MAXN];
+bool visitado[MAXN];
+
+void dfs(int pos)
+{
+    if (visitado[pos])
+    {
+        return;
+    }
+
+    // Estou marcando com true os nós visitados para evitar entrar em loop infinito
+    visitado[pos] = true; 
+
+    for (auto i : grafo[pos])
+    {
+        dfs(i);
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        grafo[u].push_back(v);
+        grafo[v].push_back(u); // Grafo não orientado
+    }
+}
