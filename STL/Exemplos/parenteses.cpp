f1b50a08d39ec564e5f1869528290641141884f0
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-10 14:59:22 -0300

    STL: Exempos
    Adicionado o Exemplo dos parenteses

---
+++ STL/Exemplos/parenteses.cpp
@@ -0,0 +1,60 @@
+// Recebe uma sequancia de () {} [] e diz se ela está correta
+
+#include <bits/stdc++.h>
+using namespace std;
+
+stack<char> parentese;
+
+void colocar(char b)
+{
+    if (b == '(' || b == '[' || b == '{')
+    {
+        parentese.push(b);
+        return;
+    }
+    if (b == ')')
+    {
+        if (parentese.empty() || parentese.top() != '(')
+        {
+            cout << "Invalido" << endl;
+            exit(0);
+        }
+        parentese.pop();
+    }
+    if (b == ']')
+    {
+        if (parentese.empty() || parentese.top() != '[')
+        {
+            cout << "Invalido" << endl;
+            exit(0);
+        }
+        parentese.pop();
+    }
+    if (b == '}')
+    {
+        if (parentese.empty() || parentese.top() != '{')
+        {
+            cout << "Invalido" << endl;
+            exit(0);
+        }
+        parentese.pop();
+    }
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    for (char c : str)
+    {
+        colocar(c);
+    }
+
+    if (parentese.empty())
+    {
+        cout << "Valido" << endl;
+        return 0;
+    }
+    cout << "Invalido" << endl;
+}
