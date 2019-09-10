0bb05e4438de28ebb98e432f0dfe18a13d9b2706
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-10 14:40:30 -0300

    STL: Vector: bound

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -31,6 +31,15 @@ void iteradores()
     cout << endl;
 }
 
+void bound()
+{
+    vector<int> v = {1, 2, 3, 3, 3, 5, 6, 6};
+    auto low = lower_bound(v.begin(), v.end(), 3); // iterador a partir do elemento menor que 3
+    auto up = upper_bound(v.begin(), v.end(), 3); // iterador a partir do elemento maior que 3
+
+    int cont = up - low;
+}
+
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 5};
@@ -64,8 +73,12 @@ int main()
     v.push_back(-1);
 
     sort(v.begin(), v.end()); // crescente
-    for (int x : v) cout << x << " "; cout << endl;
+    for (int x : v)
+        cout << x << " ";
+    cout << endl;
 
     sort(v.rbegin(), v.rend()); // decrescente
-    for (int x : v) cout << x << " "; cout << endl;
+    for (int x : v)
+        cout << x << " ";
+    cout << endl;
 }
