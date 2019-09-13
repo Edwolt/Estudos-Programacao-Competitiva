3387fe745029d9a42467a30472db4212bb6403ee
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2019-09-12 21:43:37 -0300

    Numeros: MMC MDC

---
+++ Teoria dos Numeros/mdc.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mdc(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return (mdc(b, a % b));
+}
+
+int mmc(int a, int b)
+{
+    return (a * b) / mdc(a, b);
+}
